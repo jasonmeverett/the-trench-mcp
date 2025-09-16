ab1d886e986069720da01ae00fd3103e491c47cc
Jason Everett <jev@cloudera.com>
2025-09-16 03:41:35 +0000

    more

--- src/trench_mcp/server.py
+++ src/trench_mcp/server.py
@@ -4,21 +4,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-mcp = FastMCP(name="Cloudera Iceberg MCP Server via Impala")
+mcp = FastMCP(name="The Trench MCP Server")
 
 
 # Register functions as MCP tools
 @mcp.tool()
 def example_tool(query: str) -> str:
     """
-    Execute a SQL query on the Impala database and return results as JSON.
+    Simple Example Tool
     """
     return "example tool"
 
 
 def main():
     transport = os.getenv("MCP_TRANSPORT", "stdio")
-    print(f"Starting Iceberg MCP Server via transport: {transport}")
+    print(f"Starting Trench MCP Server via transport: {transport}")
     mcp.run(transport=transport)
 
 if __name__ == "__main__":
